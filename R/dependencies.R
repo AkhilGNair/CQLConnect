d1e2211d38afd62774a425c9d5daacfc04cebf6b
Akhil Nair <Akhil.nair@amey.co.uk>
2017-02-27 19:06:38 +0000

    added scalacass jar

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,14 +1,25 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
+  message(paste("spark version:", spark_version, "| scala_version:", scala_version))
   sparklyr::spark_dependency(
     jars = c(
       system.file(
         sprintf("java/CQLConnect-%s-%s.jar", spark_version, scala_version),
         package = "CQLConnect"
+      ),
+      system.file(
+        sprintf("java/scalacass_%s-0.6.14.jar", scala_version),
+        package = "CQLConnect"
       )
     ),
     packages = c(
-      # "com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3",
-      # "datastax:spark-cassandra-connector:2.0.0-RC1-s_2.11"
+      # https://mvnrepository.com/artifact/com.google.guava/guava
+      'com.google.guava:guava:jar:16.0.1',
+      # https://mvnrepository.com/artifact/com.chuusai/shapeless_2.11
+      'com.chuusai:shapeless_2.11:jar:2.3.2',
+      # https://dl.bintray.com/thurstonsand/maven/com/github/thurstonsand/scalacass_2.11/0.6.14/
+      'com.github.thurstonsand:scalacass_2.11:0.6.14',
+      # https://mvnrepository.com/artifact/com.datastax.spark/spark-cassandra-connector_2.11
+      'com.datastax.spark:spark-cassandra-connector_2.11:jar:2.0.0-M3'
     )
   )
 }
