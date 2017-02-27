1cc3f74aa00f61290fb78dd5aa731c7ebae3deb4
AkhilNairAmey <Akhil.nair@amey.co.uk>
2017-02-27 21:18:10 +0000

    fixed maven coordinates

--- R/dependencies.R
+++ R/dependencies.R
@@ -16,8 +16,6 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
       'com.google.guava:guava:16.0.1',
       # https://mvnrepository.com/artifact/com.chuusai/shapeless_2.11
       'com.chuusai:shapeless_2.11:2.3.2',
-      # https://dl.bintray.com/thurstonsand/maven/com/github/thurstonsand/scalacass_2.11/0.6.14/
-      'com.github.thurstonsand:scalacass_2.11:0.6.14',
       # https://mvnrepository.com/artifact/com.datastax.spark/spark-cassandra-connector_2.11
       'com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3'
     )
