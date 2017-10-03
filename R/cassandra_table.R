ddc671b868068e147266af0cdc9df1c290a1e7a6
Akhil Nair <akhil.nair@amey.co.uk>
2017-10-03 19:04:48 +0100

    joinWithCassandraTable and serverside selection work

--- R/cassandra_table.R
+++ R/cassandra_table.R
@@ -10,6 +10,22 @@ NULL
 #' Get a Cassandra table
 #'
 #' @export
-cql_get_table <- function(sc, keyspace, table) {
-  sparklyr::invoke_static(sc, "CQLConnect.CassandraTable", "get_table", sc$spark_context, keyspace, table)
+cql_get_table <- function(sc, keyspace, table, select_cols = list()) {
+
+  sparklyr::invoke_static(sc, "CQLConnect.CassandraTable", "get_table", sc$spark_context, keyspace, table, select_cols)
+
+}
+
+#' Test joinWithCassandraTable
+#'
+#' @export
+cql_test_jwct <- function(sc, keyspace, table) {
+  sparklyr::invoke_static(sc, "CQLConnect.CassandraTable", "test_jwct", sc$spark_context, keyspace, table)
+}
+
+#' Show schema
+#'
+#' @export
+cql_show_schema <- function(sc, keyspace, table) {
+  sparklyr::invoke_static(sc, "CQLConnect.CassandraTable", "show_schema", sc$spark_context, keyspace, table)
 }
--- java/CassandraTable.scala
+++ java/CassandraTable.scala
@@ -1,19 +1,18 @@
 package CQLConnect
 
 import java.sql
+import java.sql.Timestamp
 import java.util.Date
 
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql._
+
 import org.apache.spark.sql.cassandra._
-import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SparkSession
-import CQLConnect.Models.DistMVFileIndexModel
-import CQLConnect.DateUtils.convert_cld_to_d
-import com.datastax.driver.core.LocalDate
+import org.apache.spark.sql.types._
+
 
 object CassandraTable {
 
@@ -22,20 +21,43 @@ object CassandraTable {
 
   def convertToSpark(element:Any): Any = element match {
     case time: org.joda.time.LocalDate => new sql.Date(time.toDateTimeAtStartOfDay().getMillis) // Convert to java.sql.Date
+    case date: java.util.Date => new Timestamp(date.getTime)
+    case uuid: java.util.UUID => uuid.toString()
     case other => other
   }
 
-  def get_table ( sc: SparkContext, keyspace: String, table: String ) = {
+
+  def get_table ( sc: SparkContext, keyspace: String, table: String, select: Array[String]) = {
+
+    var schema = spark.read.cassandraFormat(table, keyspace).load.schema
+
+    var cass_table = sc.cassandraTable(keyspace, table)
+
+    if (select.length > 0) {
+      schema = StructType(schema.filter(x => select.contains(x.name)))
+      cass_table = cass_table.select(select.map(ColumnName(_)):_*)
+    }
+
+    val spk_cass = cass_table.map{ case cassandraRow => Row(cassandraRow.columnValues.map(convertToSpark):_*) }
+
+    val dataset = spark.createDataFrame(spk_cass, schema)
+
+     dataset
+
+  }
+
+  def test_jwct ( sc: SparkContext, keyspace: String, table: String ) = {
 
     val schema = spark.read.cassandraFormat(table, keyspace).load.schema
 
     val joinResult =
-      sc.cassandraTable(keyspace, table)
-        .map{ case cassandraRow => Row(cassandraRow.columnValues.map(convertToSpark):_*)}
+      sc.cassandraTable(keyspace, "partitions_obc_model").where("date = '2017-05-13'")
+        .joinWithCassandraTable(keyspace, table)
+        .map{ case(_, cassandraRow) => Row(cassandraRow.columnValues.map(convertToSpark):_*)}
 
     val dataset = spark.createDataFrame(joinResult, schema)
 
-     dataset
+    dataset
 
   }
 
